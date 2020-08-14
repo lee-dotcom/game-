46ff56233e5f83f5e7defa2233d7a4758197ab6b
lee <lee.nepinak@connect.7oaks.org>
2020-08-14 15:05:34 -0500

    add player conditions for animation trigger  plus background asset

--- Assets/scripts/playercontorroler.cs
+++ Assets/scripts/playercontorroler.cs
@@ -8,18 +8,27 @@ public class playercontorroler : MonoBehaviour
     public Animator anim;
     public Rigidbody2D rb;
     public int jumpforce;
+    public Transform groundpoint;
+    public LayerMask groundlayer;
+    public bool grounded; 
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapPoint(groundpoint.position, groundlayer);
+            }
     // Update is called once per frame
     void Update()
-    { if(Input.GetKeyDown(KeyCode.Space))
+    { if(grounded && Input.GetKeyDown(KeyCode.Space))
         {
             anim.SetTrigger("jump");
             rb.AddForce(Vector2.up * jumpforce);
+
         }
-        
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("grounded", grounded);
     }
 }
